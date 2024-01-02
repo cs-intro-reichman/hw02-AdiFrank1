19b88746f55e90c07b3d4f63d9f7d50674008df2
AdiFrank1 <154518396+AdiFrank1@users.noreply.github.com>
2024-01-02 18:25:34 +0200

    edit

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,9 +3,10 @@ public static void main(String[] args)
 {
 int num = Integer.parseInt(args[0]);
 String z = "";
-for (int i=0;i<num;i++)
+for (int i=0;i<num-1;i++)
 	z += "* ";
 
+	z+= "*";
 
 for (int i=0; i<num;i++)
 	if (i%2!=0)
