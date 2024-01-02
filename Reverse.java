64848b48026cfdf822bb132e760f8f529df7fec9
AdiFrank1 <154518396+AdiFrank1@users.noreply.github.com>
2024-01-02 14:27:18 +0200

    edit 4

--- Reverse.java
+++ Reverse.java
@@ -12,7 +12,7 @@ for (int i=n-1; i>=0; i--)
 	}
 
 		System.out.println (z);
-		System.out.println ("the middle character is " + z.charAt(mid));
+		System.out.println ("The middle character is " + z.charAt(mid));
 
 }
 
